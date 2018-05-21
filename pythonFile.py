2c98b9eda6d77ccf66e713a727e84ff90200d3f2
jverrette <jverrette@gmail.com>
2018-05-21 14:14:30 +0200

    fixed some cases for various contact links

--- pythonFile.py
+++ pythonFile.py
@@ -1,13 +1,11 @@
 import urllib3, sys, re
 from bs4 import BeautifulSoup
 
-#soup = pythonFile.get_soup('http://tacosplaza.ch/contact-2.html')
-#pythonFile.facebook(soup)
-#pythonFile.instagram(soup)
 
 def get_soup(stringer):
     #wiki = 'http://petdoc.ch/'
     wiki = stringer
+    #print(stringer)
 #Query the website and return the html to the variable 'page'
     http = urllib3.PoolManager()
     page = http.request('GET', wiki)
@@ -26,7 +24,7 @@ def facebook(soup):
          
         except:
             pass
-    return 'None Available'
+    return ''
 
 
 def instagram(soup):
@@ -40,7 +38,7 @@ def instagram(soup):
          
         except:
             pass
-    return 'None Available'
+    return ''
 
 def db(soup):
 
@@ -62,7 +60,7 @@ def email(lister):
             return match.group(0)
         except:
             pass
-    return 'None Available'
+    return ''
 
 def phone(lister):
     usa = '(1|\+1)?([ -])?(\d{3}|\(\d{3}\))?([ -])?\d{3}([ -])?\d{4}'
@@ -75,13 +73,61 @@ def phone(lister):
                 return match.group(0)
             except:
                 pass
-    return 'None Available'
+    return ''
+
+def contact(stringer, soup):
+# Returns either:
+#1. the first link available on page with the word contact in the title
+#2. 'None Available'
+    links = soup.find_all('a') 
+    for idx, link in enumerate(links):
+        try:
+            match = re.search('(contact)|(kontakt)', link.get('href'))
+            print(link)
+            if match.group(0):
+    # Check if the link can be input to get_soup function
+                if stringerContact[:1]=='//': 
+                    return stringerContact = 'http:'+stringerContact
+                elif stringerContact[0]=='/':
+                    return stringerContact = stringer+stringerContact
+                return link.get('href')
+         
+        except:
+            pass
+    return ''
+
+def missingData(stringer, soup, listing):
+# accepts a list of strings which may have missing data
+# returns a list of strings with any additional data available included
+    
+    # boolean mask for missing information
+    missing = [not bool(word) for word in listing]
+    if any(missing):
+        stringerContact = contact(soup)
+    else:
+        stringerContact = ''
+    # perform only if the original page has some contactpage 
+    # and there is missing information
+    #print(stringerContact)
+    if bool(stringerContact):
+        functions = [email, phone, facebook, instagram]
+        try: # in case the contact link on the site is broken
+            soupContact = get_soup(stringerContact)
+            for idx in [missed for missed in range(4) if missing[missed]]:
+    # look only for missing information
+                listing[idx] = functions[idx](soupContact)
+        except:
+            pass
+    return listing
 
 def main(stringer):
     soup = get_soup(stringer)
     lister = db(soup)
-    return email(lister), phone(lister), facebook(soup), instagram(soup)
+    listing = [email(lister), phone(lister), facebook(soup), instagram(soup)]
+
+    return missingData(soup, listing)
   
 if __name__=='__main__':
     email, phone, facebook, instagram = main(sys.argv[1])
     print('Email: %s, Phone: %s, Facebook: %s, Instagram: %s' % (email, phone, facebook, instagram))
+# python3 pythonFile.py 'http://diewaid.ch/'
